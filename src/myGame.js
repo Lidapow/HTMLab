f5a347f69f677ca8a4457b2b7fa0498ec5d7fca7
Lidapow <Lidapow@users.noreply.github.com>
2015-06-06 16:53:55 +0800

    Try draw

---
+++ src/myGame.js
@@ -0,0 +1,25 @@
+(function (lib, img, cjs) {
+	
+	var p; //shourtcut to reference prototypes
+
+	lib.prperties = {
+		width: 960,
+		height: 600,
+		fps: 60,
+		color: #999,
+		manifest: [
+			{src:"img/dove.png", id:"Symbol"},
+		],
+	};
+
+	(lib.Symbol = function() {
+		this.initialize(img.Symbol);
+	}).prototype = p = new cjs.Bitmap();
+	p.nominalBounds = new cjs.Rectangle(0,0,128,128);
+
+	(lib.MyGame = function(mode, startPosition, loop) {
+		this.initialize(mode, startPosition, loop, {})
+	}).prototype = p = new cjs.MovieClie();
+	p.nominalBounds = new cjs.Rectangle(128,0,128,128);
+})(lib = lib||{}, images = image||{}, createjs = createjs||{});
+var lib, images, createjs;
