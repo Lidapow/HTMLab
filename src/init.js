5608aa64c6bcb4c50ae487c0ab798f853745002a
Lidapow <Lidapow@users.noreply.github.com>
2015-06-06 17:33:24 +0800

    test

--- src/init.js
+++ src/init.js
@@ -24,23 +24,3 @@ function handleComplete() {
 	createjs.Ticker.setFPS(lib.properties.fps);
 	createjs.Ticker.addEventListener("tick", stage);
 }
-
-(function() {
-	var stats = new Stats();
-	stats.setMode(1); // 0: fps, 1: ms
-
-	// align top-left
-	stats.domElement.style.position = 'absolute';
-	stats.domElement.style.left = '0px';
-	stats.domElement.style.top = '0px';
-
-	$("body").append( stats.domElement );
-
-	var update = function () {
-	    stats.begin();
-	    // monitored code goes here
-	    stats.end();
-	    requestAnimationFrame( update );
-	};
-	requestAnimationFrame( update );
-})();
