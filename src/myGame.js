840e7eab3d4bf32916e071f4f1186ab66044c0a7
Lidapow <Lidapow@users.noreply.github.com>
2015-06-06 17:36:21 +0800

    test

--- src/myGame.js
+++ src/myGame.js
@@ -20,6 +20,6 @@
 	(lib.MyGame = function(mode, startPosition, loop) {
 		this.initialize(mode, startPosition, loop, {})
 	}).prototype = p = new cjs.MovieClip();
-	p.nominalBounds = new cjs.Rectangle(128,0,128,128);
+	p.nominalBounds = new cjs.Rectangle(0,0,960,600);
 })(lib = lib||{}, images = images||{}, createjs = createjs||{});
 var lib, images, createjs;
